c65cb034767978a77324b14be09ab483a3744e0b
gtrevor <gtrevor@umich.edu>
2017-12-07 00:02:40 -0500

    init commit

---
+++ data/parse.py
@@ -0,0 +1,15 @@
+"""Json text parser."""
+
+import json
+
+data = None
+with open("data.txt", 'r') as f:
+    data = json.load(f)
+
+with open("data.json", 'w+') as f:
+    f.write(json.dumps(data, indent=4))
+
+for key, value in data['chart'].items():
+    file_name = 'chart-' + key + '.json'
+    with open(file_name, 'w+') as f:
+        f.write(json.dumps(value, indent=4))
